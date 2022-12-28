08e19a64d77974a87626954a22b65996502061b6
Daan-Borst <daanborst@live.nl>
2022-12-28 21:49:40 +0100

    lalala

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: daanb
+ *
+ * Created on December 28, 2022, 9:35 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
